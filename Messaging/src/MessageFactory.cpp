55adc315c1534ac8f7885c911bd1a7c10eac0bdf
toastedcode <jasontost@gmail.com>
2017-08-15 13:10:10 -0400

    Minor logging detail fix.

--- Messaging/src/MessageFactory.cpp
+++ Messaging/src/MessageFactory.cpp
@@ -22,7 +22,7 @@ MessagePtr MessageFactory::newMessage()
    if (message == 0)
    {
       // Note: Logged at DEBUG_FINEST to protect RemoteLogger from stack overflow.
-      Logger::logDebugFinest(F("Insufficient message allocation."));
+      Logger::logDebugFinest(F("MessageFactory::newMessage: Insufficient message allocation."));
    }
    else
    {
@@ -41,7 +41,7 @@ MessagePtr MessageFactory::newMessage(
    if (message == 0)
    {
       // Note: Logged at DEBUG_FINEST to protect RemoteLogger from stack overflow.
-      Logger::logDebugFinest(F("Insufficient message allocation."));
+      Logger::logDebugFinest(F("MessageFactory::newMessage: Insufficient message allocation."));
    }
    else
    {
