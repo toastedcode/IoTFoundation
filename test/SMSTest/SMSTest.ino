90cb73538fa86dd37dfb069f4bff1e3fe3b81ccf
toastedcode <jasontost@gmail.com>
2018-07-21 08:21:22 -0400

    Quick fix.

--- test/SMSTest/SMSTest.ino
+++ test/SMSTest/SMSTest.ino
@@ -11,17 +11,17 @@ void setup()
 
    Board::setBoard(new Esp8266Board());
 
-   WifiBoard::getBoard()->connectWifi("compunetix-guest", "compunetix");
+   WifiBoard::getBoard()->connectWifi("REDACTED", "REDACTED");
 
    SMS::setup(
       "api.twilio.com",  // host
       443,               // port
-      "AC47bd79771c6965a92b58d94440e8fa59",  // account sid
-      "e97eb2e67fee5d1a6f88a3f66dde0fab",    // auth token
+      "REDACTED",  // account sid
+      "REDACTED",    // auth token
       "REDACTED",        // api.twilio.com SHA1 fingerprint, this one was valid as of May 2018.
-      "+14123243446");   // from phone number
+      "REDACTED");   // from phone number
 
-   SMS::sendSMS("+17247572815", "I bless the rains down in Africa.");
+   SMS::sendSMS("REDACTED", "I bless the rains down in Africa.");
 }
 
 void loop()
